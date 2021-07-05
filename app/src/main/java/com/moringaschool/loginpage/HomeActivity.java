248820b1855b8511900363a98feb2bdea6fd013e
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-05 07:45:25 +0300

    add the new changes made

--- app/src/main/java/com/moringaschool/loginpage/HomeActivity.java
+++ app/src/main/java/com/moringaschool/loginpage/HomeActivity.java
@@ -1,4 +1,21 @@
 package com.moringaschool.loginpage;
 
-public class HomeActivity {
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    private StorageReference mStorageRef;
+    private Button BtnChoose;
+    private Button BtnUpload;
+    private ImageView imageView;
+    private Uri filepath;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
 }
