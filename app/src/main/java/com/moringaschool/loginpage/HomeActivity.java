4b51095c1f74718a860bc7db80cdce879e25305b
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-05 07:42:10 +0300

    add the changes made

---
+++ app/src/main/java/com/moringaschool/loginpage/HomeActivity.java
@@ -0,0 +1,4 @@
+package com.moringaschool.loginpage;
+
+public class HomeActivity {
+}
--- app/src/main/java/com/moringaschool/loginpage/MainActivity.java
+++ app/src/main/java/com/moringaschool/loginpage/MainActivity.java
@@ -2,11 +2,13 @@ package com.moringaschool.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -28,6 +30,50 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+
+                if(email.isEmpty()){
+
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+
+                    password.setError("Please enter password");
+                    password.requestFocus();
+                }
+                else if(email.isEmpty()&&pwd.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Fields cannot be Empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty()&&pwd.isEmpty())){
+                    mAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "You are Already Sign up with this email", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Error Occured!", Toast.LENGTH_SHORT).show();
+
+                }
             }
+        });
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this , LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
 
         }
