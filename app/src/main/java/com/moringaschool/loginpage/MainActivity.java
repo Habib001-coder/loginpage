db84b6da278d89f36a7640a85b26aa4fc56b1856
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-05 07:36:01 +0300

    add new content

--- app/src/main/java/com/moringaschool/loginpage/MainActivity.java
+++ app/src/main/java/com/moringaschool/loginpage/MainActivity.java
@@ -3,12 +3,31 @@ package com.moringaschool.loginpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText emailId , password;
+    Button btnSignUp;
+    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        mAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editEmail);
+        password = findViewById(R.id.editPassword);
+        btnSignUp = findViewById(R.id.btnSignup);
+        tvSignUp = findViewById(R.id.btnAl);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        }
