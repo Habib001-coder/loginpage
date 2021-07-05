5df05b1a11b0019246dd4abb13e804b4f4f29119
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-05 07:32:08 +0300

    working on java classes

--- app/src/main/java/com/moringaschool/loginpage/LoginActivity.java
+++ app/src/main/java/com/moringaschool/loginpage/LoginActivity.java
@@ -1,6 +1,23 @@
 package com.moringaschool.loginpage;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    EditText emailId, password;
+    Button btnSignIn;
+    TextView tvSignIn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 }
